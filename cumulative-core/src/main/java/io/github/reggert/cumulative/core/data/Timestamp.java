d935cef7cd1864861889632f4022613479e96e01
Richard W. Eggert II <richard.eggert@gmail.com>
2018-01-03 20:35:56 -0500

    split out Timestamp.Specified and Timestamp.Unspecified

--- cumulative-core/src/main/java/io/github/reggert/cumulative/core/data/Timestamp.java
+++ cumulative-core/src/main/java/io/github/reggert/cumulative/core/data/Timestamp.java
@@ -18,22 +18,104 @@ import static java.util.Objects.requireNonNull;
  * new data to Accumulo, in which case Accumulo will assign the current time when it writes to the row.
  * Timestamps will always be specified when reading from Accumulo.
  */
-public final class Timestamp implements Serializable, Comparable<Timestamp> {
+public abstract class Timestamp implements Serializable {
     private static final long serialVersionUID = 1L;
-    private final long value;
-    public static final Timestamp UNSPECIFIED = new Timestamp(Long.MAX_VALUE);
+    public static final Unspecified UNSPECIFIED = new Unspecified();
+
+
+    private Timestamp() {}
 
 
     /**
-     * Constructs a {@code Timestamp} from a numeric value.
+     * Represents a timestamp that has not been specified.
      *
-     * @param value
-     *   the numeric value of the timestamp. This will either be the number of milliseconds since the Unix
-     *   Epoch, a logical time, or {@code Long.MAX_VALUE} to indicate an unspecified value (to tell
-     *   Accumulo to use the current time).
+     * For mutations, this means to apply the current database time as the timestamp.
      */
-    public Timestamp(final long value) {
-        this.value = value;
+    public static final class Unspecified extends Timestamp {
+        private static final long serialVersionUID = 1L;
+        private Unspecified() {}
+
+        @Override
+        public String toString() {
+            return "UNSPECIFIED";
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getClass());
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            return o instanceof Unspecified;
+        }
+    }
+
+
+    /**
+     * Represents a timestamp that has been specified.
+     *
+     * This may either be an actual time in milliseconds since the Unix Epoch, or a logical time.
+     */
+    public static final class Specified extends Timestamp implements Comparable<Specified> {
+        private static final long serialVersionUID = 1L;
+        private final long value;
+
+        /**
+         * Constructs a timestamp with the specified value.
+         *
+         * @param value
+         * the value of the timestamp, which is either the number of milliseconds since the Unix Epoch,
+         * or a logical time value.
+         * @throws IllegalArgumentException if the argument is {@code Long.MAX_VALUE}.
+         */
+        public Specified(final long value) {
+            if (value == Long.MAX_VALUE) {
+                throw new IllegalArgumentException("Long.MAX_VALUE is not a valid timestamp");
+            }
+            this.value = value;
+        }
+
+        /**
+         * Returns the numeric value of this timestamp.
+         */
+        public long longValue() {
+            return value;
+        }
+
+        /**
+         * Converts this timestamp to an {@link Instant}, assuming it represents an actual time rather than
+         * a logical time.
+         *
+         * @return a new {@code Instant}.
+         */
+        public Instant toInstant() {
+            return Instant.ofEpochMilli(value);
+        }
+
+        @Override
+        public int compareTo(@Nonnull final Specified that) {
+            return Long.compare(this.value, that.value);
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+            final Specified that = (Specified) o;
+            return this.value == that.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), value);
+        }
+
+        @Override
+        public String toString() {
+            return toInstant().toString();
+        }
     }
 
 
@@ -44,8 +126,8 @@ public final class Timestamp implements Serializable, Comparable<Timestamp> {
      * a time from which to create a timestamp.
      * @return a new {@code Timestamp}.
      */
-    public static Timestamp fromInstant(@Nonnull final Instant instant) {
-        return new Timestamp(requireNonNull(instant, "instant").toEpochMilli());
+    public static Specified fromInstant(@Nonnull final Instant instant) {
+        return new Specified(requireNonNull(instant, "instant").toEpochMilli());
     }
 
 
@@ -57,76 +139,8 @@ public final class Timestamp implements Serializable, Comparable<Timestamp> {
      * @return a new {@code Timestamp}
      */
     public static Timestamp fromAccumuloKey(@Nonnull final Key accumuloKey) {
-        return new Timestamp(accumuloKey.getTimestamp());
+        return accumuloKey.getTimestamp() == Long.MAX_VALUE
+            ? UNSPECIFIED : new Specified(accumuloKey.getTimestamp());
     }
 
-
-    /**
-     * Returns the numeric value of this timestamp.
-     *
-     * @return the numeric value of the timestamp, which may be the number of milliseconds since the Unix
-     *   Epoch, a logical time, or {@code Long.MAX_VALUE} to indicate an unspecified value.
-     */
-    public long longValue() {
-        return value;
-    }
-
-
-    /**
-     * Indicates whether this {@code Timestamp} represents a specific time (logical or clock), or was left
-     * unspecified.
-     *
-     * @return {@code true} if this timestamp represents a specified value, {@code false} if unspecified.
-     */
-    public boolean isSpecified() {
-        return value != Long.MAX_VALUE;
-    }
-
-
-    /**
-     * Converts this {@code Timestamp} to an {@code Instant}, assuming its value represents a clock time.
-     *
-     * @return an {@code Instant} containing the clock time representation of this timestamp.
-     * @throws IllegalStateException if this timestamp is unspecified.
-     */
-    public Instant toInstant() {
-        if (isSpecified()) {
-            return Instant.ofEpochMilli(value);
-        }
-        else {
-            throw new IllegalStateException("Timestamp is unspecified");
-        }
-    }
-
-
-    @Override
-    public int compareTo(@Nonnull final Timestamp that) {
-        return Long.compare(this.value, that.value);
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final Timestamp that = (Timestamp) o;
-        return this.value == that.value;
-    }
-
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-
-    @Override
-    public String toString() {
-        if (value == Long.MAX_VALUE) {
-            return "UNSPECIFIED";
-        }
-        else {
-            return toInstant().toString();
-        }
-    }
 }
--- cumulative-core/src/test/scala/io/github/reggert/cumulative/core/data/TimestampTest.scala
+++ cumulative-core/src/test/scala/io/github/reggert/cumulative/core/data/TimestampTest.scala
@@ -18,12 +18,7 @@ class TimestampTest extends FunSuite with Matchers {
   test("Timestamp.fromAccumuloKey") {
     val accumuloKey = new Key(new Text("a"), new Text(""), new Text(""), new Text(""), 12345L)
     val timestamp = Timestamp.fromAccumuloKey(accumuloKey)
-    timestamp.longValue() should equal (accumuloKey.getTimestamp)
-  }
-
-  test("Timestamp.isSpecified") {
-    Timestamp.fromInstant(Instant.ofEpochMilli(123456L)).isSpecified should be (true)
-    Timestamp.UNSPECIFIED.isSpecified should be (false)
+    timestamp should equal (new Timestamp.Specified(accumuloKey.getTimestamp))
   }
 
   test("Timestamp.toString") {
@@ -34,26 +29,25 @@ class TimestampTest extends FunSuite with Matchers {
 
   test("Timestamp.equals") {
     Timestamp.UNSPECIFIED should equal (Timestamp.UNSPECIFIED)
-    Timestamp.UNSPECIFIED should equal (new Timestamp(Long.MaxValue))
     Timestamp.UNSPECIFIED should not equal (Timestamp.fromInstant(Instant.EPOCH))
     Timestamp.UNSPECIFIED should not equal ("abc")
+    val specified = new Timestamp.Specified(1L)
+    specified should equal (specified)
+    specified should equal (new Timestamp.Specified(1L))
+    specified should not equal (new Timestamp.Specified(2L))
+    specified should not equal ("abc")
   }
 
-  test("Timestamp.compareTo") {
-    Timestamp.UNSPECIFIED should be > Timestamp.fromInstant(Instant.EPOCH)
-    Timestamp.UNSPECIFIED should be <= Timestamp.UNSPECIFIED
-    new Timestamp(1L) should be < new Timestamp(2L)
+  test("Timestamp.Specified.compareTo") {
+    new Timestamp.Specified(1L) should be < new Timestamp.Specified(2L)
   }
 
   test("Timestamp.hashCode") {
-    Timestamp.UNSPECIFIED.hashCode() should equal (new Timestamp(Long.MaxValue).hashCode())
-    Timestamp.UNSPECIFIED.hashCode() should not equal (new Timestamp(0L).hashCode())
+    Timestamp.UNSPECIFIED.hashCode() should not equal (new Timestamp.Specified(0L).hashCode())
+    new Timestamp.Specified(1L).hashCode() should equal (new Timestamp.Specified(1L).hashCode())
   }
 
-  test("Timestamp.toInstant") {
-    new Timestamp(0L).toInstant should equal (Instant.EPOCH)
-    assertThrows[IllegalStateException] {
-      Timestamp.UNSPECIFIED.toInstant
-    }
+  test("Timestamp.Specified.toInstant") {
+    new Timestamp.Specified(0L).toInstant should equal (Instant.EPOCH)
   }
 }
