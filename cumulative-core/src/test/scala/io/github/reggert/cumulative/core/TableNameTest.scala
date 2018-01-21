ae7cf25aae40f51aee57a0bb1140adc5b77a979d
Richard W. Eggert II <richard.eggert@gmail.com>
2018-01-21 18:32:26 -0500

    re-created TableNameTest

---
+++ cumulative-core/src/test/scala/io/github/reggert/cumulative/core/TableNameTest.scala
@@ -0,0 +1,83 @@
+package io.github.reggert.cumulative.core
+
+import org.scalatest.{FunSuite, Matchers}
+
+
+/**
+  * Unit tests for [[TableName]].
+  */
+class TableNameTest extends FunSuite with Matchers {
+
+  test("TableName constructor accepts a valid namespace and name") {
+    val ns = "testns"
+    val name = "testname"
+    val tableName = TableName(ns, name)
+    tableName.namespace should equal (ns)
+    tableName.name should equal (name)
+  }
+
+  test("TableName constructor accepts an empty namespace and valid name") {
+    val ns = ""
+    val name = "testname"
+    val tableName = TableName(ns, name)
+    tableName.namespace should equal (ns)
+    tableName.name should equal (name)
+  }
+
+  test("TableName constructor rejects an invalid namespace") {
+    val ns = "1-.d"
+    val name = "testname"
+    assertThrows[IllegalArgumentException] {
+      TableName(ns, name)
+    }
+  }
+
+  test("TableName constructor rejects an invalid name") {
+    val ns = ""
+    val name = "@@@."
+    assertThrows[IllegalArgumentException] {
+      TableName(ns, name)
+    }
+  }
+
+  test("TableName constructor rejects an empty name") {
+    val ns = ""
+    val name = ""
+    assertThrows[IllegalArgumentException] {
+      TableName(ns, name)
+    }
+  }
+
+  test("TableName.apply accepts a valid plain name") {
+    val tableName = TableName("testname")
+    tableName.namespace shouldBe empty
+    tableName.name should equal ("testname")
+  }
+
+  test("TableName.apply accepts a valid qualified name") {
+    val tableName = TableName("testns.testname")
+    tableName.namespace should equal ("testns")
+    tableName.name should equal ("testname")
+  }
+
+  test("TableName.apply rejects an invalid qualified name") {
+    assertThrows[IllegalArgumentException] {
+      TableName("...")
+    }
+  }
+
+  test("TableName.tableNameOrdering sorts by namespace first") {
+    TableName("a.y") should be < (TableName("b.x"))
+    TableName("c.x") should be > (TableName("b.z"))
+  }
+
+  test("TableName.tableNameOrdering sorts by name second") {
+    TableName("a.x") should be < (TableName("a.y"))
+    TableName("a.z") should be > (TableName("a.y"))
+  }
+
+  test("TableName.toString") {
+    TableName("a.x").toString should equal ("a.x")
+    TableName("x").toString should equal ("x")
+  }
+}
