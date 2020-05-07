cc55a5dc3355788590f3a4dc5510c9b3b43cb024
Richard W. Eggert II <richard.eggert@gmail.com>
2020-05-07 13:27:10 -0400

    made menuBar method mimic that of RichWindow

--- cumulative-ui/src/main/scala/scala/swing/InternalFrame.scala
+++ cumulative-ui/src/main/scala/scala/swing/InternalFrame.scala
@@ -25,11 +25,10 @@ class InternalFrame extends Component with RootPanel with Publisher { outer =>
   /**
     * The menu bar of this frame or [[NoMenuBar]] if no menu bar is set.
     */
-  def menuBar: MenuBar =
-    peer.getJMenuBar match {
-      case null => NoMenuBar
-      case m => UIElement.wrap(m).asInstanceOf[MenuBar]
-    }
+  def menuBar: MenuBar = {
+    val m = UIElement.cachedWrapper[MenuBar](peer.getJMenuBar)
+    if (m != null) m else MenuBar.NoMenuBar
+  }
 
   /**
     * Set the current menu bar of this frame. Pass `NoMenuBar` if this frame
